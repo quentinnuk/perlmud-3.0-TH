74b0b095259bd55a5f651f929cdee8db3e4c8408
Quentinnuk <quentin@quentin.org.uk>
2020-05-09 01:02:05 +0100

    update to help.txt, mud.db and test.pl Updated help file represents current working help. mud.db is latest build and test.pl tests the eval calls.

--- test.pl
+++ test.pl
@@ -3,7 +3,7 @@ use Scalar::Util qw(looks_like_number);
 
 use warnings;
 
-my $instruction = 'if ( mud_ifblind($me,$arg,$arg1,$arg2,\'null\',\'1\') ) {&tellPlayer($me,$objects[0]{"msg1"}); }';
+my $instruction = 'if ( mud_ifdumb($me,$arg,$arg1,$arg2,\'null\',\'1\') ) {&flush($me,$arg,$arg1,$arg2); &tellPlayer($me,$objects[0]{"msg1"}); 1; }';
 my @objects;
 my ($me,$arg,$arg1,$arg2) = qw (0 123 1 2);
 
@@ -14,8 +14,9 @@ print "eval instruction\n";
 my $a = eval $instruction;
 print "returned $a \n";
 print "eval complete\n";
-print "inline\n";
-if ( mud_testsmall($me,$arg,$arg1,$arg2,'null') ) {&tellPlayer($me,$objects[0]{"msg1"}); } else { &tellPlayer($me,$objects[0]{"msg2"}); }print "inline complete\n";
+#print "inline\n";
+#if ( mud_testsmall($me,$arg,$arg1,$arg2,'null') ) {&tellPlayer($me,$objects[0]{"msg1"}); } else { &tellPlayer($me,$objects[0]{"msg2"}); }
+#print "inline complete\n";
     
 sub mud_testsmall
 {
@@ -28,7 +29,20 @@ sub mud_ifblind
 {
     my ($me,$arg,$arg1,$arg2,$farg1,$farg2) = @_;
     print "mud_ifblind\n";
-#    return 0; # true
+    return 0; # false
+}
+
+sub mud_ifdumb
+{
+    my ($me,$arg,$arg1,$arg2,$farg1,$farg2) = @_;
+    print "mud_ifdumb\n";
+    return 0; # false
+}
+
+sub flush
+{
+    my ($me,$msg) = @_;
+    print "flush\n";
 }
 
 sub tellPlayer
